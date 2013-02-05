65bce12a1e0d754a8d15c9263609234a07fc8830
Sol <info@solarianprogrammer.com>
2013-02-05 09:09:31 -0500

    Update parallel_sort_GPU.cu

--- parallel_sort_GPU.cu
+++ parallel_sort_GPU.cu
@@ -8,25 +8,6 @@
 #include <thrust/host_vector.h>
 #include <thrust/sort.h>
 
-//Split "mem" into "parts", e.g. if mem = 10 and parts = 4 you will have: 0,2,4,6,10
-//if possible the function will split mem into equal chuncks, if not 
-//the last chunck will be slightly larger
-std::vector<size_t> bounds(size_t parts, size_t mem) {
-    std::vector<size_t>bnd(parts + 1);
-    size_t delta = mem / parts;
-    size_t reminder = mem % parts;
-    size_t N1 = 0, N2 = 0;
-    bnd[0] = N1;
-    for (size_t i = 0; i < parts; ++i) {
-        N2 = N1 + delta;
-        if (i == parts - 1)
-            N2 += reminder;
-        bnd[i + 1] = N2;
-        N1 = N2;
-    }
-    return bnd;
-}
-
 //Fill a vector with random numbers in the range [lower, upper]
 void rnd_fill(thrust::host_vector<double> &V, const double lower, const double upper, const unsigned int seed) {
 
@@ -68,7 +49,7 @@ int main() {
 	    //Start recording
 	    cudaEventRecord(start,0);
         
-        thrust::stable_sort(d_V.begin(), d_V.end());
+            thrust::stable_sort(d_V.begin(), d_V.end());
         
 	    //Stop recording
 	    cudaEventRecord(stop,0);
